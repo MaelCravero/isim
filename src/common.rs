182c9234eb29774b4ffb580fb53048f6c1441783
Mael Cravero <mael.cravero@epita.fr>
2021-02-25 22:02:58 +0100

    common: add frequent color aliases

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,12 @@
 #[derive(Copy, Clone, Debug)]
 pub struct Color(pub u8, pub u8, pub u8);
 
+pub const BLACK: Color = Color(0, 0, 0);
+pub const WHITE: Color = Color(255, 255, 255);
+pub const RED: Color = Color(255, 0, 0);
+pub const GREEN: Color = Color(0, 255, 0);
+pub const BLUE: Color = Color(0, 0, 255);
+
 #[derive(Copy, Clone, Debug)]
 pub struct Point(pub f64, pub f64, pub f64);
 
