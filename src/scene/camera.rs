ee3364983f4c189bf390d9521c67dad85e337693
Mael Cravero <mael.cravero@epita.fr>
2021-02-27 11:59:30 +0100

    camera: refactor struct and get_pos method

--- src/scene/camera.rs
+++ src/scene/camera.rs
@@ -5,13 +5,12 @@ use crate::{
 
 pub struct Camera {
     pub pos: Point,
-    center_of_view: Point,
-    up: Vector,
-    x_fov_angle: f64,
-    y_fov_angle: f64,
     pub z_min: f64,
     pub height: usize,
     pub width: usize,
+    top_left: Vector,
+    vunit_x: Vector,
+    vunit_y: Vector,
 }
 
 impl Camera {
@@ -25,37 +24,32 @@ impl Camera {
         height: usize,
         width: usize,
     ) -> Self {
+        let w = 2.0 * z_min * (y_fov_angle / 2.0).to_radians().tan();
+        let h = 2.0 * z_min * (x_fov_angle / 2.0).to_radians().tan();
+
+        let unit_y = w / (width as f64);
+        let unit_x = h / (height as f64);
+        let vunit_y =
+            Vector::cross_product(&up, &Vector::from(center_of_view, pos)).normalize() * unit_y;
+        let vunit_x = up * unit_x;
+
+        let vec_center = Vector::from(ORIGIN, center_of_view).normalize() * z_min;
+
+        let top_left =
+            vec_center + vunit_y * (width as f64 / 2.0) + vunit_x * (height as f64 / 2.0);
         Camera {
             pos,
-            center_of_view,
-            up,
-            x_fov_angle,
-            y_fov_angle,
             z_min,
             height,
             width,
+            top_left,
+            vunit_x,
+            vunit_y,
         }
     }
 
     pub fn get_pixel_pos(&self, x: usize, y: usize) -> Point {
-        let w = 2.0 * self.z_min * (self.y_fov_angle / 2.0).to_radians().tan();
-        let h = 2.0 * self.z_min * (self.x_fov_angle / 2.0).to_radians().tan();
-
-        let unit_y = w / (self.width as f64);
-        let unit_x = h / (self.height as f64);
-        let vunit_y =
-            Vector::cross_product(&self.up, &Vector::from(self.center_of_view, self.pos))
-                .normalize()
-                * unit_y;
-        let vunit_x = self.up * unit_x;
-
-        let vec_center = Vector::from(ORIGIN, self.center_of_view).normalize() * self.z_min;
-
-        let top_left = vec_center
-            + vunit_y * (self.width as f64 / 2.0)
-            + vunit_x * (self.height as f64 / 2.0);
-
-        let pos = top_left - vunit_x * (x as f64) - vunit_y * (y as f64);
+        let pos = self.top_left - self.vunit_x * (x as f64) - self.vunit_y * (y as f64);
 
         Point(pos.x, pos.y, pos.z)
     }
