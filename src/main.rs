536d6a36c50a2d164d6dfb5d3c3983a78fca4a6f
Mael Cravero <mael.cravero@epita.fr>
2021-02-28 23:41:58 +0100

    Add scene1 module and rework main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod engine;
 mod geometry;
 mod image;
 mod scene;
+mod scene1;
 
 use std::fs::File;
 use std::path::Path;
@@ -29,48 +30,34 @@ fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
     let cam = scene::Camera::new(
-        common::ORIGIN,
-        Point(0.0, 0.0, 1.0),
+        Point(0.0, 0.0, 0.0),
+        Point(0.0, 0.0, 8.0),
         Vector::new(1.0, 0.0, 0.0),
         90.0,
         90.0,
-        5.0,
+        2.0,
         400,
         400,
     );
-
+    let mut lights = Vec::<Box<dyn scene::Light>>::new();
     let mut objs = Vec::<Box<dyn scene::Object>>::new();
+
     let texture = scene::texture::UniformTexture::new(common::RED, 1.0, 1.0);
     objs.push(Box::new(
-        scene::Sphere::<scene::texture::UniformTexture>::new(Point(0.0, 0.0, 6.0), 1.0, texture),
-    ));
-    let texture = scene::texture::UniformTexture::new(common::GREEN, 1.0, 1.0);
-    objs.push(Box::new(
-        scene::Sphere::<scene::texture::UniformTexture>::new(Point(0.0, 0.0, 8.0), 4.0, texture),
-    ));
-    let texture = scene::texture::UniformTexture::new(common::BLUE, 1.0, 1.0);
-    objs.push(Box::new(
-        scene::Sphere::<scene::texture::UniformTexture>::new(Point(1.0, 1.0, 7.0), 1.0, texture),
-    ));
-    let texture = scene::texture::UniformTexture::new(common::WHITE, 1.0, 1.0);
-    objs.push(Box::new(
-        scene::Sphere::<scene::texture::UniformTexture>::new(Point(-3.0, -3.0, 6.0), 1.0, texture),
+        scene::Sphere::<scene::texture::UniformTexture>::new(Point(0.0, 0.0, 8.0), 1.0, texture),
     ));
 
-    // "Skybox"
-    let texture = scene::texture::UniformTexture::new(Color(135, 206, 235), 1.0, 1.0);
-    objs.push(Box::new(
-        scene::Sphere::<scene::texture::UniformTexture>::new(common::ORIGIN, 1000.0, texture),
-    ));
-
-    let mut lights = Vec::<Box<dyn scene::Light>>::new();
     lights.push(Box::new(scene::light::PointLight::new(
-        Point(4.0, 4.0, 6.0),
+        Point(0.0, 0.0, 0.0),
         (1.0, 1.0, 1.0),
     )));
 
     let scene = scene::Scene::new(cam, lights, objs);
-    let engine = engine::Engine::new(scene);
+
+    let mut engine = engine::Engine::new(scene);
+
+    engine.set_diffuse().set_specular();
+
     let image = engine.render();
 
     save_image(&args[1], image)
