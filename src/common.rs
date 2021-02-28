fb9302a26f275c6d86cc8e830a566e7221d0b4aa
Mael Cravero <mael.cravero@epita.fr>
2021-02-28 18:25:38 +0100

    common: add additions for colors

--- src/common.rs
+++ src/common.rs
@@ -7,6 +7,34 @@ pub const RED: Color = Color(255, 0, 0);
 pub const GREEN: Color = Color(0, 255, 0);
 pub const BLUE: Color = Color(0, 0, 255);
 
+fn add_color(a: u8, b: u8) -> u8 {
+    match a.overflowing_add(b) {
+        (v, false) => v,
+        (_, true) => u8::MAX,
+    }
+}
+
+impl std::ops::Add for Color {
+    type Output = Color;
+    fn add(self, rhs: Color) -> Self::Output {
+        Color(
+            add_color(self.0, rhs.0),
+            add_color(self.1, rhs.1),
+            add_color(self.2, rhs.2),
+        )
+    }
+}
+
+impl std::ops::AddAssign for Color {
+    fn add_assign(&mut self, rhs: Color) {
+        *self = Color(
+            add_color(self.0, rhs.0),
+            add_color(self.1, rhs.1),
+            add_color(self.2, rhs.2),
+        )
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub struct Point(pub f64, pub f64, pub f64);
 
