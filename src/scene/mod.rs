203dd1b7779cbdd864bcf6b25c7be889a0673186
Mael Cravero <mael.cravero@epita.fr>
2021-03-15 10:29:43 +0100

    scene: add macros and type aliases

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -9,8 +9,13 @@ use crate::{common::Point, geometry::Vector};
 
 pub use camera::Camera;
 pub use ray::Ray;
-pub use scene::Scene;
 pub use sphere::Sphere;
+
+pub use scene::LightContainer;
+pub use scene::LightType;
+pub use scene::ObjectContainer;
+pub use scene::ObjectType;
+pub use scene::Scene;
 
 pub trait TextureMaterial {
     fn diffusion(&self, p: Point) -> (f64, f64, f64);
@@ -27,4 +32,22 @@ pub trait Object {
 pub trait Light {
     fn pos(&self) -> Point;
     fn intensity(&self) -> (f64, f64, f64);
+}
+
+#[macro_export]
+macro_rules! sphere {
+    (($x:expr, $y:expr, $z:expr); $r:expr; <uniform>($c:expr, $d:expr, $s:expr)) => {{
+        use crate::common::Point;
+        use crate::scene::texture::UniformTexture;
+        use crate::scene::Sphere;
+
+        Sphere::<UniformTexture>::new(Point($x, $y, $z), $r, UniformTexture::new($c, $d, $s))
+    }};
+
+    ($p:expr; $r:expr; <uniform>($c:expr, $d:expr, $s:expr)) => {{
+        use crate::scene::texture::UniformTexture;
+        use crate::scene::Sphere;
+
+        Sphere::<UniformTexture>::new($p, $r, UniformTexture::new($c, $d, $s))
+    }};
 }
