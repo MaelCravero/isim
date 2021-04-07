3bc5537b40e648a95248b69dc7d16554794e7f71
Mael Cravero <mael.cravero@epita.fr>
2021-04-07 18:05:11 +0200

    vector: fix critical typo

--- src/geometry/vector.rs
+++ src/geometry/vector.rs
@@ -72,7 +72,7 @@ impl Vector {
         Vector::new(
             v1.y * v2.z - v1.z * v2.y,
             v1.z * v2.x - v1.x * v2.z,
-            v1.x * v2.y - v1.y * v1.x,
+            v1.x * v2.y - v1.y * v2.x,
         )
     }
 
--- src/main.rs
+++ src/main.rs
@@ -29,37 +29,41 @@ fn save_image(path: &str, image: Image) {
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
-    let (res_x, res_y) = (200, 200);
+    let (res_x, res_y) = (700, 700);
     let cam = scene::Camera::new(
         Point(0.0, 0.0, 0.0),
         Point(0.0, 0.0, 4.0),
         Vector::new(1.0, 0.0, 0.0),
         90.0,
         90.0,
-        5.0,
+        1.0,
         res_x,
         res_y,
     );
     let mut lights = scene::LightContainer::new();
     let mut objs: scene::ObjectContainer = vec![
-        //Box::new(sphere! {(0.0, 1.5, 11.0); 1.0; <uniform>(common::RED, 1.0, 1.0)}),
-        //Box::new(sphere! {(0.0, -0.5, 9.0); 0.5; <uniform>(common::GREEN, 1.0, 1.0)}),
+        Box::new(sphere! {(0.0, 1.5, 11.0); 1.0; <uniform>(common::RED, 1.0, 1.0)}),
+        Box::new(sphere! {(0.0, -0.5, 9.0); 0.5; <uniform>(common::GREEN, 1.0, 1.0)}),
         // Aligned
         //Box::new(sphere! {(0.0, -3.0, 8.0); 1.0; <uniform>(common::RED, 1.0, 1.0)}),
         //Box::new(sphere! {(0.0, 3.0, 8.0); 2.0; <uniform>(common::GREEN, 1.0, 1.0)}),
+        //Box::new(sphere! {(0.0, 0.0, 18.0); 2.0; <uniform>(common::GREEN, 1.0, 1.0)}),
+        //Box::new(sphere! {(2.0, 0.0, 14.9); 0.2; <uniform>(common::RED, 1.0, 1.0)}),
+        //Box::new(sphere! {(0.0, -2.0, 14.9); 0.2; <uniform>(common::RED, 1.0, 1.0)}),
+        //Box::new(sphere! {(0.0, 2.0, 14.9); 0.2; <uniform>(common::RED, 1.0, 1.0)}),
     ];
 
     objs.push(Box::new(scene::Triangle::new(
         (
-            Point(1.0, 0.0, 6.0),
-            Point(0.0, -1.0, 5.0),
-            Point(0.0, 1.0, 5.0),
+            Point(10.0, 0.0, 16.0),
+            Point(0.0, -10.0, 15.0),
+            Point(-10.0, 10.0, 12.0),
         ),
-        scene::texture::UniformTexture::new(common::BLUE, 1.0, 1.0),
+        scene::texture::UniformTexture::new(common::BLUE, 1.0, 0.5),
     )));
 
     lights.push(Box::new(scene::light::PointLight::new(
-        Point(0.0, 0.0, 0.0),
+        Point(-2.0, 1.0, 6.0),
         (1.0, 1.0, 1.0),
     )));
 
@@ -68,11 +72,11 @@ fn main() {
     let mut engine = engine::Engine::new(scene);
     //let mut engine = engine::Engine::new(scene1::get(res_x, res_y));
 
-    //engine.set_diffuse();
-    //engine.set_specular();
-    //engine.set_ambient((0.1, 0.1, 0.12));
+    engine.set_diffuse();
+    engine.set_specular();
+    engine.set_ambient((0.1, 0.1, 0.12));
     //engine.set_reflection();
-    engine.set_intersect();
+    //engine.set_intersect();
 
     let image = engine.render();
 
