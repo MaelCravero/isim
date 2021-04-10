0080a03decfa82cd4c0ed988f8639109d54702b0
Mael Cravero <mael.cravero@epita.fr>
2021-04-10 16:17:10 +0200

    lsystem: refacto translation

--- src/lsystem.rs
+++ src/lsystem.rs
@@ -44,63 +44,123 @@ impl LSystem {
             .collect::<Vec<char>>()
             .to_owned();
     }
-
-    pub fn translate(
-        &self,
-        pos: Point,
-        d_theta: f64,
-        direction: NormalVector,
-        length: f64,
-        radius: f64,
-    ) -> crate::scene::ObjectContainer {
-        let mut res = crate::scene::ObjectContainer::new();
-        let mut pos = pos;
-        let mut stack = Vec::new();
-        let mut theta: f64 = 0.0;
-
-        for v in self.value.iter() {
-            match v {
-                'X' => (),
-                'F' => {
-                    use crate::scene::texture::UniformTexture;
-                    use crate::scene::Cylinder;
-
-                    let Vector { x, y, z } = direction.vector();
-                    let direction = Vector::new(
-                        theta.cos() * x - theta.sin() * y,
-                        theta.sin() * x + theta.cos() * y,
-                        z,
-                    );
-                    let dst = (Vector::from(ORIGIN, pos) + length * direction).to_point();
-
-                    res.push(Box::new(Cylinder::new(
-                        pos,
-                        dst,
-                        radius,
-                        UniformTexture::new(GREEN, 1.0, 1.0),
-                    )));
-
-                    pos = dst;
-                }
-                '+' => theta += d_theta,
-                '-' => theta -= d_theta,
-                '[' => stack.push((pos, theta)),
-                ']' => {
-                    let pair = stack.pop().unwrap();
-                    pos = pair.0;
-                    theta = pair.1;
-                }
-                c => panic!("Unallowed char {}", c),
-            }
-        }
-
-        res
-    }
 }
 
 impl ToString for LSystem {
     fn to_string(&self) -> String {
         self.value.iter().collect()
+    }
+}
+
+impl LSystem {
+    pub fn translate(
+        &self,
+        pos: Point,
+        d_theta: f64,
+        d_roll: f64,
+        d_pitch: f64,
+        direction: NormalVector,
+        length: f64,
+        radius: f64,
+    ) -> crate::scene::ObjectContainer {
+        let state = LSTState {
+            pos,
+            theta: 0.0,
+            roll: 0.0,
+            pitch: 0.0,
+        };
+        LSTranslator::new(direction, d_theta, d_roll, d_pitch, length, radius)
+            .run(state, &self.value)
+    }
+}
+
+use crate::scene::ObjectContainer as LSTResult;
+
+#[derive(Debug, Clone, Copy)]
+struct LSTState {
+    pos: Point,
+    theta: f64,
+    roll: f64,
+    pitch: f64,
+}
+
+type LSTStack = Vec<LSTState>;
+
+struct LSTranslator {
+    direction: NormalVector,
+    d_theta: f64,
+    d_roll: f64,
+    d_pitch: f64,
+    length: f64,
+    radius: f64,
+    saved_states: LSTStack,
+    res: LSTResult,
+}
+
+impl LSTranslator {
+    fn new(
+        direction: NormalVector,
+        d_theta: f64,
+        d_roll: f64,
+        d_pitch: f64,
+        length: f64,
+        radius: f64,
+    ) -> LSTranslator {
+        LSTranslator {
+            direction,
+            d_theta,
+            d_roll,
+            d_pitch,
+            length,
+            radius,
+            saved_states: LSTStack::new(),
+            res: LSTResult::new(),
+        }
+    }
+
+    fn add_edge(&mut self, state: &LSTState, dst: Point) {
+        use crate::scene::texture::UniformTexture;
+        use crate::scene::Cylinder;
+
+        self.res.push(Box::new(Cylinder::new(
+            state.pos,
+            dst,
+            self.radius,
+            UniformTexture::new(GREEN, 1.0, 1.0),
+        )));
+    }
+
+    fn compute_dst(&self, state: &LSTState) -> Point {
+        let Vector { x, y, z } = self.direction.vector();
+        let direction = Vector::new(
+            state.theta.cos() * x - state.theta.sin() * y,
+            state.theta.sin() * x + state.theta.cos() * y,
+            z,
+        );
+        let dst = (Vector::from(ORIGIN, state.pos) + self.length * direction).to_point();
+        dst
+    }
+
+    fn run(mut self, initial_state: LSTState, values: &LSValues) -> LSTResult {
+        let mut state = initial_state;
+
+        for val in values {
+            match val {
+                'x' | 'X' => (),
+                'f' | 'F' => {
+                    let dst = self.compute_dst(&state);
+                    self.add_edge(&state, dst);
+                    state.pos = dst;
+                }
+                '+' => state.theta += self.d_theta,
+                '-' => state.theta -= self.d_theta,
+                '[' => self.saved_states.push(state.clone()),
+                ']' => state = self.saved_states.pop().unwrap(),
+                c => panic!("Unallowed char {}", c),
+            }
+        }
+
+        self.res
     }
 }
 
