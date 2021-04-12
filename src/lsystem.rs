169515f08efafc681717d187c094f71c22ad1fd6
Mael Cravero <mael.cravero@epita.fr>
2021-04-12 10:26:46 +0200

    lsystem: refacto translation

--- src/lsystem.rs
+++ src/lsystem.rs
@@ -89,14 +89,8 @@ impl LSystem {
         length: f64,
         radius: f64,
     ) -> crate::scene::ObjectContainer {
-        let state = LSTState {
-            pos,
-            turn: 0.0,
-            roll: 0.0,
-            pitch: 0.0,
-        };
-        LSTranslator::new(direction, d_turn, d_roll, d_pitch, length, radius)
-            .run(state, &self.value)
+        let state = LSTState { pos, direction };
+        LSTranslator::new(d_turn, d_roll, d_pitch, length, radius).run(state, &self.value)
     }
 }
 
@@ -105,15 +99,44 @@ use crate::scene::ObjectContainer as LSTResult;
 #[derive(Debug, Clone, Copy)]
 struct LSTState {
     pos: Point,
-    turn: f64,
-    roll: f64,
-    pitch: f64,
+    direction: NormalVector,
+}
+
+impl LSTState {
+    fn rotate_turn(&mut self, turn: f64) {
+        let Vector { x, y, z } = self.direction.vector();
+        let (x, y, z) = (
+            turn.cos() * x - turn.sin() * y,
+            turn.sin() * x + turn.cos() * y,
+            z,
+        );
+        self.direction = Vector::new(x, y, z).normalize();
+    }
+
+    fn rotate_pitch(&mut self, pitch: f64) {
+        let Vector { x, y, z } = self.direction.vector();
+        let (x, y, z) = (
+            pitch.cos() * x + pitch.sin() * z,
+            y,
+            -pitch.sin() * x + pitch.cos() * z,
+        );
+        self.direction = Vector::new(x, y, z).normalize();
+    }
+
+    fn rotate_roll(&mut self, roll: f64) {
+        let Vector { x, y, z } = self.direction.vector();
+        let (x, y, z) = (
+            x,
+            roll.cos() * y - roll.sin() * z,
+            roll.sin() * y + roll.cos() * z,
+        );
+        self.direction = Vector::new(x, y, z).normalize();
+    }
 }
 
 type LSTStack = Vec<LSTState>;
 
 struct LSTranslator {
-    direction: NormalVector,
     d_turn: f64,
     d_roll: f64,
     d_pitch: f64,
@@ -124,16 +147,8 @@ struct LSTranslator {
 }
 
 impl LSTranslator {
-    fn new(
-        direction: NormalVector,
-        d_turn: f64,
-        d_roll: f64,
-        d_pitch: f64,
-        length: f64,
-        radius: f64,
-    ) -> LSTranslator {
+    fn new(d_turn: f64, d_roll: f64, d_pitch: f64, length: f64, radius: f64) -> LSTranslator {
         LSTranslator {
-            direction,
             d_turn,
             d_roll,
             d_pitch,
@@ -156,30 +171,9 @@ impl LSTranslator {
         )));
     }
 
-    fn rotate(x: f64, y: f64, z: f64, state: &LSTState) -> (f64, f64, f64) {
-        let (x, y, z) = (
-            state.turn.cos() * x - state.turn.sin() * y,
-            state.turn.sin() * x + state.turn.cos() * y,
-            z,
-        );
-        let (x, y, z) = (
-            state.pitch.cos() * x + state.pitch.sin() * z,
-            y,
-            -state.pitch.sin() * x + state.pitch.cos() * z,
-        );
-        let (x, y, z) = (
-            x,
-            state.roll.cos() * y - state.roll.sin() * z,
-            state.roll.sin() * y + state.roll.cos() * z,
-        );
-        (x, y, z)
-    }
-
     fn compute_dst(&self, state: &LSTState) -> Point {
-        let Vector { x, y, z } = self.direction.vector();
-        let (x, y, z) = LSTranslator::rotate(x, y, z, &state);
-        let direction = Vector::new(x, y, z);
-        let dst = (Vector::from(ORIGIN, state.pos) + self.length * direction).to_point();
+        let dst =
+            (Vector::from(ORIGIN, state.pos) + self.length * state.direction.vector()).to_point();
         dst
     }
 
@@ -188,21 +182,21 @@ impl LSTranslator {
 
         for val in values {
             match val {
-                'x' | 'X' => (),
                 'f' | 'F' => {
                     let dst = self.compute_dst(&state);
                     self.add_edge(&state, dst);
                     state.pos = dst;
                 }
-                '+' => state.turn += self.d_turn,
-                '-' => state.turn -= self.d_turn,
-                '&' => state.pitch += self.d_pitch,
-                '^' => state.pitch -= self.d_pitch,
-                '\\' => state.roll += self.d_roll,
-                '/' => state.roll -= self.d_roll,
-                '|' => state.turn += 180f64.to_radians(),
+                '+' => state.rotate_turn(self.d_turn),
+                '-' => state.rotate_turn(-self.d_turn),
+                '&' => state.rotate_pitch(self.d_pitch),
+                '^' => state.rotate_pitch(-self.d_pitch),
+                '\\' => state.rotate_roll(self.d_roll),
+                '/' => state.rotate_roll(-self.d_roll),
+                '|' => state.rotate_turn(180f64.to_radians()),
                 '[' => self.saved_states.push(state.clone()),
                 ']' => state = self.saved_states.pop().unwrap(),
+                _ => (),
                 c => panic!("Unallowed char {}", c),
             }
         }
